72a08d95164093314450aeab77a319ebadc5556b
Aashna Sofat <asofat_be19@thapar.edu>
2021-06-30 00:23:20 +0530

    Modal Window practice in javascript.

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let a = 0; a < btnsOpenModal.length; a++) {
+  btnsOpenModal[a].addEventListener('click', openModal);
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  let answer = modal.classList.contains('hidden');
+
+  if (event.key === 'Escape' && !answer) {
+    closeModal();
+  }
+});
